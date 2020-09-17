334846e50d3078dc2964c00324ed8264b4e7abcf
deepbodra96 <deepbodra96@gmail.com>
2020-09-17 14:30:43 -0400

    command line args work

--- proj1.fsx
+++ proj1.fsx
@@ -1,36 +1,15 @@
 #time "on"
 #r "nuget: Akka.FSharp" 
 #r "nuget: Akka.Remote"
-#r "nuget: Akka.TestKit"
 
-open System
 open Akka.Actor
 open Akka.Configuration
 open Akka.FSharp
-// open Akka.TestKit
 
+// actors will be shutdown automatically after 50s to free the resources
 let configuration = 
     ConfigurationFactory.ParseString(
         @"akka {
-            // log-config-on-start : on
-            // stdout-loglevel : DEBUG
-            // loglevel : ERROR
-            actor {
-                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
-                debug : {
-                    receive : on
-                    autoreceive : on
-                    lifecycle : on
-                    event-stream : on
-                    unhandled : on
-                }
-            }
-            remote {
-                helios.tcp {
-                    port = 8777
-                    hostname = localhost
-                }
-            }
             coordinated-shutdown {
                 phases {    
                     actor-system-terminate {
@@ -41,50 +20,52 @@ let configuration =
             }
         }")
 
-
+// Job is assigned by sending this message
 type JobParams = {
-    start: float
-    stop: float
-    step: float
-    nActors: float
+    start: float // start value of n. for this problem it is 1
+    stop: float // end value of n. for this problem it is n
+    step: float // k
+    nActors: float // no of akka actors
 }
 
+// different message types
 type Message =
     | JobParams of JobParams
 
-let main() =
-    use system = ActorSystem.Create("Project1", configuration)
-    let child (childMailbox: Actor<Message>) = 
+// main program
+let main start stop step nActors =
+    use system = ActorSystem.Create("Project1", configuration) // create an actor systems
+    let child (childMailbox: Actor<Message>) = // worker actor (child)
         actor {    
-            let! msg = childMailbox.Receive()
+            let! msg = childMailbox.Receive() // fetch the message from the queue
             match msg with
-            | JobParams(param) ->
-                let SumOfConsecutiveSquare n =
+            | JobParams(param) -> // if it is a job
+                let SumOfConsecutiveSquare n = // sum of n consecutive squares = n * (n+1) * (2n+1) / 6
                     n * (n+1.0) * (2.0 * n + 1.0) / 6.0
                 
-                let IsPerfectSquare n =
-                    let flooredSquare = n |> double |> sqrt |> floor |> int
-                    n =  flooredSquare * flooredSquare
+                let IsPerfectSquare n = // checks if n is a perfect square or not
+                    let flooredSquareRoot = n |> double |> sqrt |> floor |> int
+                    n =  flooredSquareRoot * flooredSquareRoot // perfect square if floored square root is equal to n
 
-                let ConsecutivePerfectSquareCumulativeSum start stop step =
+                let ConsecutivePerfectSquareCumulativeSum start stop step = // works on a sub task
                     for start in [start .. stop] do
                         let isPerfectSquare = SumOfConsecutiveSquare(start+step-1.0) - SumOfConsecutiveSquare(start-1.0) |> int |> IsPerfectSquare
                         match isPerfectSquare with
                         | true -> printfn "%d" <| int start
                         | false -> printf ""
-                ConsecutivePerfectSquareCumulativeSum param.start param.stop param.step
+                ConsecutivePerfectSquareCumulativeSum param.start param.stop param.step // perform job
         }
 
-    let parent = 
+    let parent = // job assignment actor (parent, supervisor)
         spawnOpt system "parent"
             <| fun parentMailbox ->
                 let rec parentLoop() =
                     actor {
-                        let! (msg: Message) = parentMailbox.Receive()
+                        let! (msg: Message) = parentMailbox.Receive() // fetch the message from the queue
                         match msg with
-                        | JobParams param ->
+                        | JobParams param -> // if it is a job
                             let workUnit = (param.stop - param.start + 1.0) / param.nActors |> int |> float
-                            let extraWorkUnit = (param.stop - param.start + 1.0) % param.nActors |> int |> float
+                            let extraWorkUnit = (param.stop - param.start + 1.0) % param.nActors |> int |> float // remainder work unit
                             let mutable tempStart = param.start
                             for i in [1.0 .. param.nActors] do
                                 let childRef = spawn parentMailbox ("child" + string i) child
@@ -100,22 +81,18 @@ let main() =
                     }
                 parentLoop()
             
+            // default supervisor strategy
             <| [ SpawnOption.SupervisorStrategy (
                     Strategy.OneForOne(fun e ->
                     match e with 
                     | _ -> SupervisorStrategy.DefaultDecider.Decide(e)))  ]
 
-    // async {
-        // let param = JobParams {start=1.0 ; stop=40.0 ; step=24.0; nActors=4.0}
-        // let param = JobParams {start=1.0 ; stop=1000000.0 ; step=24.0; nActors=4.0}
-        // let param = JobParams {start=1.0 ; stop=100000000.0 ; step=2.0; nActors=4.0}
-        // let param = JobParams {start=1.0 ; stop=100000000000.0 ; step=24.0; nActors=10.0}
-        // parent <! param
-        // system.Terminate()
-    // } |> Async.RunSynchronously |> ignore
-    let param = JobParams {start=1.0 ; stop=100000000.0 ; step=2.0; nActors=4.0}
-    // let param = JobParams {start=1.0 ; stop=100000000.0 ; step=20.0; nActors=8.0}
+    let param = JobParams {start=start ; stop=stop ; step=step; nActors=nActors}
     parent <! param |> ignore
     system.Terminate()
 
-main ()
+let args : string array = fsi.CommandLineArgs |> Array.tail
+let n = float args.[0]
+let k = float args.[1]
+let nActors = 8.0
+main 1.0 n k nActors
