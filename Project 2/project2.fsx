b6750f85cc30b1bb1fb9e59c045abb3492d61e17
deepbodra96 <deepbodra96@gmail.com>
2020-10-14 11:32:34 -0400

    2d, imp2d, full-converges

--- Project 2/project2.fsx
+++ Project 2/project2.fsx
@@ -40,6 +40,7 @@ let main n topology algorithm =
 
         let mutable value = 0.0
         let mutable weight = 0.0
+        let mutable consecutive = 0
 
         // For 2D and imp2D
         let getLeftNeighbor = if id % dim = 1 then -1 else (id-1)
@@ -87,14 +88,22 @@ let main n topology algorithm =
                         system.Scheduler.ScheduleTellRepeatedly(TimeSpan.Zero, (TimeSpan.FromMilliseconds(1.0)), childMailbox.Self, (TickPushSum "tick"))
                     messageCount <- messageCount + 1
                     // if messageCount = 10 then
-                        // system.ActorSelection("/user/parent") <! Done "done"
+                    //     system.ActorSelection("/user/parent") <! Done "done"
+                    if abs (value/weight - (value+newValue)/(weight+newWeight)) < 10.0**(-10.0) then
+                        consecutive <- consecutive + 1
+                        if consecutive = 5 then
+                            printfn "Converged: %f" (value/weight)
+                            system.ActorSelection("/user/parent") <! Done "done"
+                    else
+                        consecutive <- 0
+
                     value <- value + newValue
                     weight <- weight + newWeight
                 | TickPushSum tick ->
                     value <- value / 2.0
                     weight <- weight / 2.0
                     system.ActorSelection("/user/parent/"+chooseNeighbor ()) <! PushSum(value, weight)
-                    printfn "Sum=%f" (value/weight)
+                    // printfn "Sum=%f" (value/weight)
                 | _ -> printfn "Invalid message"
                 return! childLoop()
             }
@@ -119,15 +128,19 @@ let main n topology algorithm =
                             for i in 1 .. numNodes do
                                 let childRef = spawn parentMailbox (string i) child
                                 childRef <! ValueWeightInit (float i, 0.0)
-                            let startRef = system.ActorSelection("/user/parent/"+ string(getRandomInt 1 1))
-                            startRef <! ValueWeightInit (float 1, 1.0)
+                            let startId = getRandomInt 1 numNodes
+                            let startRef = system.ActorSelection("/user/parent/"+ string(startId))
+                            startRef <! ValueWeightInit (float startId, 1.0)
                             startRef <! PushSum(s, w)
                         | Done done_msg ->
                             printfn "Parent received done %O" sender
                             messageCount <- messageCount + 1
-                            // if messageCount = numNodes then
+                            if messageCount = numNodes then
+                                printfn "----------------------------------------------------"
                                 // system.ActorSelection("/user/parent") <! PoisonPill.Instance
                                 // parentMailbox.Context.Stop(parentMailbox.Self)
+                                system.Terminate()
+                                // mainSender <! "Done"
                         return! parentLoop()
                     }
                 parentLoop()
@@ -160,7 +173,17 @@ let main n topology algorithm =
 // main 5 "imp2D" "gossip"
 // main 1000 "full" "gossip"
 
-// main 100 "line" "pushsum"
-// main 1000 "2D" "pushsum"
-// main 5 "imp2D" "pushsum"
-main 55 "full" "pushsum"
+// main 100 "line" "pushsum" // 15=15
+// main 20 "line" "pushsum" // 15=15
+
+
+// 2D works
+// main 121 "2D" "pushsum" // 7381=7381
+
+// imp2D works
+// main 16 "imp2D" "pushsum" // 136=136
+// main 121 "imp2D" "pushsum" // 7381=7381
+
+// full works
+// main 5 "full" "pushsum" // 5050
+// main 1000 "full" "pushsum" // 500500
