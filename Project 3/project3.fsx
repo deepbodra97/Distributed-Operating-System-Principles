971a23039a11405b781d558c732a6b4020e06180
deepbodra96 <deepbodra96@gmail.com>
2020-11-02 15:58:37 -0500

    p3 final

--- Project 3/project3.fsx
+++ Project 3/project3.fsx
@@ -7,34 +7,34 @@ open System.Diagnostics
 
 // different message types
 type Message =
-    | Start of string
-    | StartRequestPhase
-    | Join of string
-    | JoinSuccess
-    | NewRow of int * string[]
-    | NewLeaves of Set<string>
-    | Route of string * string * int
-    | RouteSuccess of int
-    | RequestTick
+    | Start of string // parent starts spawning nodes. Nodes start joining
+    | StartRequestPhase // Nodes start making 1 request per second
+    | Join of string // route the Join packet
+    | JoinSuccess // parent know that a node has finished joining
+    | NewRow of int * string[] //  (row number, row of routing table)
+    | NewLeaves of Set<string> // leaf set
+    | Route of string * string * int // route the Route(request) packet
+    | RouteSuccess of int // report number of hops to parent
+    | RequestTick // tick every 1 second
 
 // main program
 let main numNodes numRequests =
-    use system = ActorSystem.Create("Project3") // create an actor systems
+    use system = ActorSystem.Create("Project3") // create an actor system
 
     let getRandomInt start stop =  // get random integer [start, stop]
         let rnd = System.Random()
         rnd.Next(start, stop+1)
 
-    let decimalTo (x: int) (b: int) =
+    let decimalTo (x: int) (b: int) = // convert an int to base b (b=16)
         System.Convert.ToString(x, b).PadLeft(8, '0')
-    let toDecimal (x: string) =
+    let toDecimal (x: string) = // from hexadecimal to decimal
         System.Convert.ToInt32(x, 16)    
     
     let hexToDecimalMap = Map.empty.Add('0', 0).Add('1', 1).Add('2', 2).Add('3', 3).Add('4', 4).Add('5', 5).Add('6', 6).Add('7', 7).Add('8', 8).Add('9', 9).Add('a', 10).Add('b', 11).Add('c', 12).Add('d', 13).Add('e', 14).Add('f', 15)
     let hexToDecimal (c:char) =
         hexToDecimalMap.[c]
     
-    let longestCommonPrefix (s1:string) (s2:string) =
+    let longestCommonPrefix (s1:string) (s2:string) = // find longest common prefix
         let n1 = s1.Length
         let n2 = s2.Length
         let rec loop i j =
@@ -46,41 +46,41 @@ let main numNodes numRequests =
         if index = -1 then ""
         else s1.[0..index]
 
-    let findClosest id (set:Set<string>) =
+    let findClosest id (set:Set<string>) = // find the node that is closest to id from set
         set
         |> Seq.mapi (fun i v -> v, abs(toDecimal(v)-toDecimal(id)))
         |> Seq.minBy snd
         |> fst
 
-    let child (childMailbox: Actor<_>) = // nodes participating in gossip or pushsum
-        let id = childMailbox.Self.Path.Name // id
-        let mutable cancelable = Unchecked.defaultof<ICancelable>
+    let child (childMailbox: Actor<_>) = // pastry node
+        let id = childMailbox.Self.Path.Name // id (hexadecimal)
+        let mutable cancelable = Unchecked.defaultof<ICancelable> // to cancel making requests
 
-        let routingTable = Array2D.create 16 16 ""
-        let mutable largerLeaves = Set.empty
-        let mutable smallerLeaves = Set.empty
+        let routingTable = Array2D.create 16 16 "" // routing table (16x16)
+        let mutable largerLeaves = Set.empty // large leaves
+        let mutable smallerLeaves = Set.empty // small leaves
 
-        let mutable nRequests = 0
+        let mutable nRequests = 0 // number of requests made by this node
 
-        let updateSmallerLeaves newNode =
+        let updateSmallerLeaves newNode = // update smalle leaf set
             if smallerLeaves.IsEmpty || smallerLeaves.Count < 8 then
                 smallerLeaves <- smallerLeaves.Add newNode
             elif smallerLeaves.Count = 8 then
                 if newNode > smallerLeaves.MinimumElement then
                     smallerLeaves <- smallerLeaves.Remove smallerLeaves.MinimumElement
         
-        let updateLargerLeaves newNode =
+        let updateLargerLeaves newNode = // update large leaf set
             if largerLeaves.IsEmpty || largerLeaves.Count < 8 then
                 largerLeaves <- largerLeaves.Add newNode
             elif largerLeaves.Count = 8 then
                 if newNode < largerLeaves.MaximumElement then
                     largerLeaves <- largerLeaves.Remove largerLeaves.MaximumElement
 
-        let sendLeaves destination =
+        let sendLeaves destination = // send leaf set to destination
             let leaves = Set.union smallerLeaves largerLeaves
             system.ActorSelection("/user/parent/"+destination) <! NewLeaves (leaves)
         
-        let getRowCol destination =
+        let getRowCol destination = //  find matching row and column
             let commonPrefix = longestCommonPrefix id destination
             let row = String.length commonPrefix
             if String.length(commonPrefix) < destination.Length then
@@ -89,28 +89,25 @@ let main numNodes numRequests =
             else
                 row, 16
 
-        let lookupRoutingTable destination =
+        let lookupRoutingTable destination = // find next hop node from routing table for a given destination
             let (row, col) = getRowCol destination
             routingTable.[row, col]
 
-        let sendMatchingRows destination =
+        let sendMatchingRows destination = // send a row from routing table
             let (row, _) = getRowCol destination
             for i in 0 .. row do
                 system.ActorSelection("/user/parent/"+destination) <! NewRow (row, routingTable.[row, *].Clone() :?> string[])
 
-        let forward source destination nHops =
+        let forward source destination nHops = // 
             let nextNode = lookupRoutingTable destination
             if nextNode = "" then
-                if destination < id && smallerLeaves.Count > 0 then
-                    // printfn "No entry in routing table. Using smaller leaves"
+                if destination < id && smallerLeaves.Count > 0 then // No entry in routing table. Using smaller leaves
                     system.ActorSelection("/user/parent/"+smallerLeaves.MinimumElement) <! Route (source, destination, nHops+1)
-                elif destination > id && largerLeaves.Count > 0 then
-                    // printfn "No entry in routing table. Using larger leaves"
+                elif destination > id && largerLeaves.Count > 0 then // No entry in routing table. Using larger leaves
                     system.ActorSelection("/user/parent/"+largerLeaves.MaximumElement) <! Route (source, destination, nHops+1)
-                else
-                    // printfn "Node %s cannont route request for %s %A %A" id destination smallerLeaves largerLeaves
+                else // Consume the message
                     system.ActorSelection("/user/parent") <! RouteSuccess (nHops+1)
-            else
+            else // next node found from the table
                 system.ActorSelection("/user/parent/"+nextNode) <! Route (source, destination, nHops+1)
 
         let rec childLoop() =
@@ -119,81 +116,65 @@ let main numNodes numRequests =
                 let sender = childMailbox.Sender()
                 match msg with
                 | Start start ->
-                    // printfn "Child %s's coordinator is %s" id start
                     if start <> "" then
                         system.ActorSelection("/user/parent/"+start) <! Join id
                     else
                         system.ActorSelection("/user/parent") <! JoinSuccess
                 | StartRequestPhase ->
-                    // printfn "StartRequestPhase %s" id
-                    // printfn "%s %A %A" id smallerLeaves largerLeaves
-                    // printfn "%s %A" id routingTable
+                    // start making 1 request per second
                     cancelable <- system.Scheduler.ScheduleTellRepeatedlyCancelable(TimeSpan.Zero, (TimeSpan.FromMilliseconds(1000.0)), childMailbox.Self, (RequestTick), childMailbox.Self)
                 | Join newNode ->
-                    if id = newNode then
-                        // printfn "%s got its join message back" id
+                    if id = newNode then // Join message reached the original sender
                         system.ActorSelection("/user/parent") <! JoinSuccess
                     else
-                        // printfn "%s joined" newNode
-                        if (lookupRoutingTable newNode) = "" then
+                        if (lookupRoutingTable newNode) = "" then // if no entry then add entry using prefix match
                             let (row, col) = getRowCol newNode
                             routingTable.[row, col] <- newNode
-                        // printfn "%d %d" row col
-                        // let mutable nextNode = "" 
-                        if newNode < id then
+                        if newNode < id then // add to small leaf set
                             updateSmallerLeaves newNode
                             system.ActorSelection("/user/parent/"+(findClosest id smallerLeaves)) <! Join newNode
-                        elif newNode > id then
+                        elif newNode > id then  // add to large leaf set
                             updateLargerLeaves newNode
                             system.ActorSelection("/user/parent/"+(findClosest id largerLeaves)) <! Join newNode
-                        sendMatchingRows newNode
-                        sendLeaves newNode
-                        // printfn "%s %A %A" id smallerLeaves largerLeaves
-                        // printfn "%s %A" id routingTable
-                | NewRow (row_num, row) ->
-                    // printfn "NewRow %A" row
+                        sendMatchingRows newNode // send rows from routing table
+                        sendLeaves newNode // send leaf set
+                | NewRow (row_num, row) -> // add the new row to the routing table
                     for i in 0 .. row.Length-1 do
                         if routingTable.[row_num, i] = "" then
                             routingTable.[row_num, i] <- row.[i]
-                | NewLeaves newLeaves ->
+                | NewLeaves newLeaves -> //  add the nodes to the leaf set
                     for leaf in newLeaves do
                         if leaf <> id then
                             if leaf < id then updateSmallerLeaves leaf
                             else updateLargerLeaves leaf
-                | RequestTick ->
-                    // printfn "RequestTick %s" id
+                | RequestTick -> // make a request now
                     nRequests <- nRequests + 1
                     system.ActorSelection("/user/parent/"+id) <! Route (id, decimalTo (getRandomInt 1 numNodes) 16, -1)
-                    // system.ActorSelection("/user/parent/"+id) <! Route (id, (decimalTo 1 16), -1)
                     if nRequests = numRequests then
-                        cancelable.Cancel ()
+                        cancelable.Cancel () // stop making request
                 | Route (source, destination, nHops) ->
-                    // printfn "Child %s received route" id
-                    if id = destination then
-                        // printfn "%s consumed a message" id
+                    if id = destination then // consume message
                         system.ActorSelection("/user/parent") <! RouteSuccess (nHops+1)
                     else
                         if destination < id then
                             if smallerLeaves.Count > 0 then
                                 if abs(toDecimal(id)-toDecimal(destination)) < abs(toDecimal(findClosest destination smallerLeaves)-toDecimal(destination)) then
-                                    // printfn "Nearest %s consumed a message" id
-                                    system.ActorSelection("/user/parent") <! RouteSuccess (nHops+1)
-                                elif destination >= smallerLeaves.MinimumElement then
+                                    system.ActorSelection("/user/parent") <! RouteSuccess (nHops+1) // consume if I am the destination is closest to me (of all the nodes in small leafset)
+                                elif destination >= smallerLeaves.MinimumElement then // if destination is in the range of small leafset
                                     system.ActorSelection("/user/parent/"+(findClosest id smallerLeaves)) <! Route (source, destination, nHops+1)
-                                else
-                                    forward source destination nHops
-                            else
+                                else // use routing table
+                                    forward source destination nHops 
+                            else // use routing table
                                 forward source destination nHops
                         if destination > id then
                             if largerLeaves.Count > 0 then
                                 if abs(toDecimal(id)-toDecimal(destination)) < abs(toDecimal(findClosest destination largerLeaves)-toDecimal(destination)) then
-                                    // printfn "%s consumed a message" id
-                                    system.ActorSelection("/user/parent") <! RouteSuccess (nHops+1)
-                                elif destination <= largerLeaves.MaximumElement then
+                                    system.ActorSelection("/user/parent") <! RouteSuccess (nHops+1) // consume if I am the destination is closest to me (of all the nodes in large leafset)
+                                elif destination <= largerLeaves.MaximumElement then // if destination is in the range of small leafset
                                     system.ActorSelection("/user/parent/"+(findClosest id largerLeaves)) <! Route (source, destination, nHops+1)
-                                else
+                                else // use routing table
                                     forward source destination nHops
-                            else
+                            else // use routing table
                                 forward source destination nHops
                 | _ -> return ()
                 return! childLoop()
@@ -204,17 +185,17 @@ let main numNodes numRequests =
     let parent = // job assignment actor (parent, supervisor)
         spawnOpt system "parent"
             <| fun parentMailbox ->
-                let mutable mainSender = Unchecked.defaultof<IActorRef>
+                let mutable mainSender = Unchecked.defaultof<IActorRef> // main program
 
-                let activeNodes = Array.create numNodes ""
-                let mutable nSuccessfulJoins = 0
-                let mutable nSuccessfulRequests = 0
-                let mutable nHops = 0
+                let activeNodes = Array.create numNodes "" // nodes currently alive in the network
+                let mutable nSuccessfulJoins = 0 // number of nodes that have joined
+                let mutable nSuccessfulRequests = 0 // number of requests served so far
+                let mutable nHops = 0 // total number of hop so far
                 
-                let maxId = 10000000
+                let maxId = 10000000 // max node id
                 let set = [| for i in 1 .. maxId -> i |]
                 let mutable lastIndex = maxId-1
-                let getUniqueRandomNumber () =
+                let getUniqueRandomNumber () = // get unique id for nodes
                     let idx = getRandomInt 0 lastIndex
                     let picked = set.[idx]
                     set.[idx] <- set.[lastIndex]
@@ -227,8 +208,8 @@ let main numNodes numRequests =
                         let! (msg: Message) = parentMailbox.Receive() // fetch the message from the queue
                         let sender = parentMailbox.Sender()
                         match msg with
-                        | Start start ->
-                            printfn "Parent received start"
+                        | Start start -> // spawn pastry nodes one by one
+                            printfn "Join phase: Please wait for all the nodes to join the network"
                             mainSender <- sender
                             for i in 1 .. numNodes do
                                 let id = decimalTo (getUniqueRandomNumber ()) 16
@@ -239,13 +220,13 @@ let main numNodes numRequests =
                                 else
                                     let poc = activeNodes.[getRandomInt 0 (i-2)] 
                                     childRef <! Start poc
-                        | JoinSuccess ->
+                        | JoinSuccess -> // a node has joined successfully
                             nSuccessfulJoins <- nSuccessfulJoins + 1
                             if nSuccessfulJoins = numNodes then
                                 printfn "All nodes have joined and routing tables have converged"
+                                printfn "Request phase: Nodes will now make 1 request per second"
                                 system.ActorSelection("/user/parent/*") <! StartRequestPhase
-                        | RouteSuccess hops->
-                            // printfn "RouteSuccess %d" hops
+                        | RouteSuccess hops-> // a request has been served
                             nSuccessfulRequests <- nSuccessfulRequests + 1
                             nHops <- nHops + hops
                             if nSuccessfulRequests = (numRequests * numNodes) then
@@ -266,15 +247,11 @@ let main numNodes numRequests =
 
 
     async {
-        // let timer = new Stopwatch()
-        // timer.Start()
         let! response = parent <? Start "0"
-        // printfn "Convergence Time: %s ms" <| timer.ElapsedMilliseconds.ToString()
-        printfn "Exit"
+        printfn ""
     } |> Async.RunSynchronously
 
 let args : string array = fsi.CommandLineArgs |> Array.tail
 let numNodes = int args.[0]
 let numRequests = int args.[1]
-// let algorithm = args.[2]
 main numNodes numRequests
