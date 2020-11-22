d5b2a16b574f5d58d80cd070a5810766913b40a7
deepbodra96 <deepbodra96@gmail.com>
2020-11-21 20:57:17 -0500

    comments

--- Project 4.1/client.fsx
+++ Project 4.1/client.fsx
@@ -44,28 +44,28 @@ type Tweet = {
 
 type Query = {
     qType: string // subscription | hashtag | mention
-    qName: string
+    qName: string // "" for subscription | "#<hashtag>" for hashtag | "@<username>" for mention
     by: User
 }
 
 type Subscribe = {
-    publisher: string
-    subscriber: string
+    publisher: string // publishers username
+    subscriber: string // subscribers username
 }
 
 // different message types
 type Message =
-    | Start of string // parent starts spawning nodes. Nodes start joining
-    | Register of User
-    | Login of User
-    | Logout of User
-    | UserBehavior of string
-    | Tweet of Tweet
-    | LiveTweet of Tweet
-    | Query of Query
-    | QueryResponse of Tweet array
-    | Subscribe of Subscribe
-    | Tick
+    | Start of string // parent starts spawning clients
+    | Register of User // Client Register
+    | Login of User // client logins / connects
+    | Logout of User // client logouts / disconnects
+    | UserBehavior of string // publisher | reader | lazy
+    | Tweet of Tweet // to tweet
+    | LiveTweet of Tweet // new tweet when the user is online
+    | Query of Query // query for tweets
+    | QueryResponse of Tweet array // response to the Query
+    | Subscribe of Subscribe // to subscribe to a user
+    | Tick // take one of the [login, logout, tweet, retweet, query] actions
     | PrintStats // not used for client
 
 // main program
@@ -73,49 +73,50 @@ let main numNodes =
     let system = ActorSystem.Create("ClientSimulator", configuration) // create an actor system
 
     // User Behaviour Parameters
-    let behaviorPercentPublisher = [|5; 15; 80|] // [disconnect, query, tweet]
-    let tickRangePublisher = [|5000; 10000|]
+    let behaviorPercentPublisher = [|5; 15; 80|] // [disconnect, query, tweet] for aggressive publisher 
+    let tickRangePublisher = [|5000; 10000|] // take an action every 5-10 seconds
 
-    let behaviorPercentReader = [|5; 80; 15|]
-    let tickRangeReader = [|5000; 10000|]
+    let behaviorPercentReader = [|5; 80; 15|] // for aggressive reader
+    let tickRangeReader = [|5000; 10000|] // take an action every 5-10 seconds
 
     let behaviorPercentLazy = [|30; 35; 35|]
-    let tickRangeLazy = [|10000; 15000|]
+    let tickRangeLazy = [|10000; 15000|] // take an action every 10-15 seconds
 
-    let maxLenTweetId = 3
+    let maxLenTweetId = 3 // max string length for a tweet's id
 
-    let mutable usernames = Array.empty
-    let mutable zipfConstant = 0.0
+    let mutable zipfConstant = 0.0 // summation of 1/i  for i in range [1,...numNodes]
 
-    let queryTypes = [|"subscription"; "hashtag"; "mention"|]
+    let queryTypes = [|"subscription"; "hashtag"; "mention"|] // different query types
+
+    let mutable usernames = Array.empty // all usernames
 
     let getRandomInt start stop =  // get random integer [start, stop]
         let rnd = System.Random()
         rnd.Next(start, stop+1)
     
-    let getRandomString n =
+    let getRandomString n = // generate random alphnumeric string of length n
         let rnd = System.Random()
         let chars = Array.concat([[|'a' .. 'z'|];[|'A' .. 'Z'|];[|'0' .. '9'|]])
         let sz = Array.length chars in
         System.String(Array.init n (fun _ -> chars.[rnd.Next sz]))
 
-    let getRandomHashTag () = 
+    let getRandomHashTag () = // generate random hashtag
             if (getRandomInt 0 1) = 0 then
                 "#" + getRandomString (getRandomInt 3 4)
             else
                 ""
     
-    let getRandomMention () = 
+    let getRandomMention () = // generate random hashtag
             if (getRandomInt 0 1) = 0 then
                 "@" + usernames.[(getRandomInt 0 (usernames.Length-1))]
             else
                 ""
 
-    let getRandomTweet () =
+    let getRandomTweet () = // generate random tweet
         let tweet = (getRandomString(getRandomInt 10 20)) + " " + getRandomMention ()+ " " + getRandomHashTag ()
         tweet
 
-    let getRandomQuery user () =
+    let getRandomQuery user () = // generate random query
         let qType = queryTypes.[(getRandomInt 0 (queryTypes.Length-1))]
         match qType with
         | "subscription" -> Query{qType=qType; qName=""; by=user}
@@ -123,30 +124,29 @@ let main numNodes =
         | "mention" -> Query{qType=qType; qName=(getRandomMention ()); by=user}
 
     let client (childMailbox: Actor<_>) = // client node
-        let mId = childMailbox.Self.Path.Name // id
-        // let mutable cancelable = Unchecked.defaultof<ICancelable> // to cancel making requests
-        let mServer = system.ActorSelection("akka.tcp://twitter@127.0.0.1:9001/user/server")
+        let mId = childMailbox.Self.Path.Name // id of client [1,..numNodes]
+        let mServer = system.ActorSelection("akka.tcp://twitter@127.0.0.1:9001/user/server") // reference to server
 
         let mutable mUser = Unchecked.defaultof<User>
         let mutable mBehavior = ""
-        let mutable mActive = true
-        let mSelf = system.ActorSelection("/user/parent/"+mId)
+        let mutable mActive = true // to simulate connection/disconnection with/from the servers
+        let mSelf = system.ActorSelection("/user/parent/"+mId) // reference to self to send Tick
 
-        let mLogin () =
+        let mLogin () = // send login tick to self
             printfn "User %s is logging in" mUser.username
             mActive <- true
             mSelf <! Login mUser
 
-        let mLogout () =
+        let mLogout () = // send logout tick to self
             printfn "User %s is logging out" mUser.username
             mActive <- false
             mSelf <! Logout mUser
 
-        let mQuery () =
+        let mQuery () = // send query tick to self
             printfn "User %s is querying" mUser.username
             mSelf <! getRandomQuery mUser
 
-        let mTweet () =
+        let mTweet () = // send tweet tick to self
             if (getRandomInt 0 1) = 0 then
                 let tweetText = getRandomTweet ()
                 printfn "User %s is tweeting: %s" mUser.username tweetText
@@ -161,39 +161,38 @@ let main numNodes =
                 let! msg = childMailbox.Receive() // fetch the message from the queue
                 let sender = childMailbox.Sender()
                 match msg with
-                | Register user -> // register is of type User
+                | Register user -> // register with the server
                     mUser <- user
                     mServer <! Register mUser
-                | Login user ->
+                | Login user -> // login into the server
                     mServer <! Login user
-                | Logout user ->
+                | Logout user -> // logout from the server
                     mServer <! Logout user
-                | UserBehavior behavior ->
+                | UserBehavior behavior -> // set simulation behavior of this user
                     mBehavior <- behavior
                     printfn "User %s is %s" mUser.username mBehavior
                 | Subscribe s ->
-                    let numToSubscribe = int(zipfConstant*float(numNodes)/float(mUser.id))
-                    for i in 1 .. numToSubscribe do
+                    let numToSubscribe = int(zipfConstant*float(numNodes)/float(mUser.id)) // find number of users to subscribe to using zipf distribution
+                    for i in 1 .. numToSubscribe do // subscribe to the users
                         let publisher = usernames.[(getRandomInt 0 (usernames.Length-1))]
                         // printfn "User %s is subscribing to %s" mUser.username publisher
                         let subscribe = {s with publisher=publisher; subscriber=mUser.username}
                         mServer <! Subscribe subscribe
-                | Tweet tweet ->
+                | Tweet tweet -> // post a tweet
                     mServer <! Tweet tweet
-                | LiveTweet tweet ->
+                | LiveTweet tweet -> // got a live tweet from the server
                     printfn "User %s received live tweet: %s" mUser.id tweet.text
-                | Query query ->
+                | Query query -> // query the server for tweets
                     mServer <! Query query
-                | QueryResponse response ->
+                | QueryResponse response -> // response from the server for a query
                     for tweet in response do
                         printfn "User %s received response: %s" mUser.username tweet.text
-                | Tick ->
+                | Tick -> // decide what action to take based on the random number and user behavior
                     let rnd = getRandomInt 1 100
                     match mBehavior with
-                    | "publisher" ->
-                        let self = system.ActorSelection("/user/parent/"+mId)
+                    | "publisher" -> // aggressive publisher behavior
                         if mActive then
-                            if rnd <= behaviorPercentPublisher.[0] then // logout with p=0.05
+                            if rnd <= behaviorPercentPublisher.[0] then // logout with p=0.05 (p=probability)
                                 mLogout ()
                             elif rnd <= (behaviorPercentPublisher.[1]-behaviorPercentPublisher.[0]) then // query with p=0.15
                                 mQuery ()
@@ -204,8 +203,7 @@ let main numNodes =
                                 mLogin ()
                         let nextTickTime = getRandomInt tickRangePublisher.[0] tickRangePublisher.[1] |> float
                         system.Scheduler.ScheduleTellRepeatedlyCancelable(TimeSpan.Zero, (TimeSpan.FromMilliseconds(nextTickTime)), childMailbox.Self, (Tick), childMailbox.Self) |> ignore
-                    | "reader" ->
-                        let self = system.ActorSelection("/user/parent/"+mId)
+                    | "reader" -> // aggressive reader behavior
                         if mActive then
                             if rnd <= behaviorPercentReader.[0] then // logout with p=0.05
                                 mLogout ()
@@ -218,8 +216,7 @@ let main numNodes =
                                 mLogin ()
                         let nextTickTime = getRandomInt tickRangeReader.[0] tickRangeReader.[1] |> float
                         system.Scheduler.ScheduleTellRepeatedlyCancelable(TimeSpan.Zero, (TimeSpan.FromMilliseconds(nextTickTime)), childMailbox.Self, (Tick), childMailbox.Self) |> ignore
-                    | "lazy" ->
-                        let self = system.ActorSelection("/user/parent/"+mId)
+                    | "lazy" -> // lazy behavior
                         if mActive then
                             if rnd <= behaviorPercentLazy.[0] then // logout with p=0.30
                                 mLogout ()
@@ -240,14 +237,14 @@ let main numNodes =
             }
         clientLoop()
 
-    let parent = // job assignment actor (parent, supervisor)
+    let parent = // parent
         spawnOpt system "parent"
             <| fun parentMailbox ->
                 let mutable mainSender = Unchecked.defaultof<IActorRef> // main program
 
-                let lazyPercent = 80
-                let agressivePublisherPercent = 10
-                let agressiveReaderPercent = 10
+                let lazyPercent = 80 // 80% of the users are lazy
+                let agressivePublisherPercent = 10 // 10% of the users are aggressive publishers
+                let agressiveReaderPercent = 10 // 10% of the users are aggressive readers
 
                 let rec parentLoop() =
                     actor {
@@ -256,15 +253,16 @@ let main numNodes =
                         match msg with
                         | Start start ->
                             for i in 1 .. numNodes do
-                                zipfConstant <- zipfConstant + 1.0/float(i)
+                                zipfConstant <- zipfConstant + 1.0/float(i) // zipf constant calculation
 
-                                let clientRef = spawn parentMailbox (string i) (client)
+                                let clientRef = spawn parentMailbox (string i) (client) // new client
                                 let username = getRandomString 5
                                 let password = getRandomString 8
                                 usernames <- Array.append usernames [|username|]
                                 let user = {id=string i; username=username; password=password}
-                                clientRef <! Register user
+                                clientRef <! Register user // ask this client to register with the server
 
+                                // set user behavior
                                 if i <= lazyPercent*numNodes/100 then
                                     clientRef <! UserBehavior "lazy"
                                 elif i <= (lazyPercent+agressivePublisherPercent)*numNodes/100 then
@@ -272,7 +270,7 @@ let main numNodes =
                                 else
                                     clientRef <! UserBehavior "reader"
                             
-                            // Subscribe                            
+                            // Ask the users to subscribe                            
                             let subscribe = {publisher=""; subscriber=""}
                             system.ActorSelection("/user/parent/*") <! Subscribe subscribe
                             system.ActorSelection("/user/parent/*") <! Tick              
--- Project 4.1/server.fsx
+++ Project 4.1/server.fsx
@@ -57,27 +57,27 @@ type Subscribe = {
 
 // different message types
 type Message =
-    | Start of string // parent starts spawning nodes. Nodes start joining
-    | Register of User
-    | Login of User
-    | Logout of User
-    | UserBehavior of string
-    | Tweet of Tweet
-    | LiveTweet of Tweet // not used for server
-    | Query of Query
-    | QueryResponse of Tweet array
-    | Subscribe of Subscribe
-    | Tick // not used for server
-    | PrintStats
+    | Start of string // parent starts spawning clients
+    | Register of User // Client Register
+    | Login of User // client logins / connects
+    | Logout of User // client logouts / disconnects
+    | UserBehavior of string // publisher | reader | lazy
+    | Tweet of Tweet // to tweet
+    | LiveTweet of Tweet // new tweet when the user is online
+    | Query of Query // query for tweets
+    | QueryResponse of Tweet array // response to the Query
+    | Subscribe of Subscribe // to subscribe to a user
+    | Tick // take one of the [login, logout, tweet, retweet, query] actions
+    | PrintStats // not used for client
 
 // main program
 let main () =
     let system = ActorSystem.Create("twitter", configuration) // create an actor system
 
-    let statsInterval = 5.0 // in seconds
+    let statsInterval = 5.0 // time interval in seconds to print stats
 
-    let regexHashTag = "#[A-Za-z0-9]+"
-    let regexMention = "@[A-Za-z0-9]+"
+    let regexHashTag = "#[A-Za-z0-9]+" // regex for hashtag
+    let regexMention = "@[A-Za-z0-9]+" // regex for mention
 
     let (|Regex|_|) pattern input =
         let matches = Regex.Matches(input, pattern)
@@ -92,42 +92,45 @@ let main () =
         spawn system "server"
             <| fun mailbox ->
                 let id = mailbox.Self.Path.Name // id
-                let clientSimulatorAddress = "akka.tcp://ClientSimulator@127.0.0.1:9002/user/parent/"
-
-                let mutable onlineUsers: Set<string> = Set.empty
-                let mutable usersMap: Map<string, User> = Map.empty
+                let clientSimulatorAddress = "akka.tcp://ClientSimulator@127.0.0.1:9002/user/parent/" // clients address
+                
+                // in memory Database
+                let mutable onlineUsers: Set<string> = Set.empty // set of usernames of online users
+                let mutable usersMap: Map<string, User> = Map.empty // username -> User
                 let mutable subscriptionsMap: Map<string, Set<string>> = Map.empty // subscriptions of a given user
                 let mutable subscribersMap: Map<string, string array> = Map.empty // subscribers of a given user
-                let mutable tweetsMap: Map<string, Tweet> = Map.empty
-                let mutable tweetsByUsername: Map<string, string array> = Map.empty
-                let mutable tweetsByHashTag: Map<string, string array> = Map.empty
-                let mutable tweetsByMention: Map<string, string array> = Map.empty
+                let mutable tweetsMap: Map<string, Tweet> = Map.empty // tweetId -> Tweet
+                let mutable tweetsByUsername: Map<string, string array> = Map.empty // username -> tweetId
+                let mutable tweetsByHashTag: Map<string, string array> = Map.empty // hashtag -> tweetId
+                let mutable tweetsByMention: Map<string, string array> = Map.empty // mention -> tweetId
 
                 // Stats
-                let mutable lastTweetCount = 0.0
-                let mutable currentTweetCount = 0.0
-                let mutable totalUsers = 0.0
+                let mutable lastTweetCount = 0.0 // previous tweet count
+                let mutable currentTweetCount = 0.0 // current tweet count
+                let mutable totalUsers = 0.0 // total no of users
 
-                let addTweet tweet =
-                    tweetsMap <- tweetsMap.Add(tweet.id, tweet)
+                let addTweet tweet = // add this tweet to the database
+                    tweetsMap <- tweetsMap.Add(tweet.id, tweet) // add to tweets Map
                     let mutable tweetIds = if tweetsByUsername.ContainsKey(tweet.by.username) then tweetsByUsername.Item(tweet.by.username) else [||]
                     tweetIds <- Array.append tweetIds [|tweet.id|]
-                    tweetsByUsername <- tweetsByUsername.Add(tweet.by.username, tweetIds)
+                    tweetsByUsername <- tweetsByUsername.Add(tweet.by.username, tweetIds) // add to tweetsByUsername
 
                     let hashTags = getPatternMatches regexHashTag tweet.text
                     let mentions = getPatternMatches regexMention tweet.text
                     
+                    // add to tweetsByHashTag
                     for tag in hashTags do
                         let mutable tweetIds = if tweetsByHashTag.ContainsKey(tag) then tweetsByHashTag.Item(tag) else [||]
                         tweetIds <- Array.append tweetIds [|tweet.id|]
                         tweetsByHashTag <- tweetsByHashTag.Add(tag, tweetIds)
                     
+                    // add to tweetsByMention
                     for mention in mentions do
                         let mutable tweetIds = if tweetsByMention.ContainsKey(mention) then tweetsByMention.Item(mention) else [||]
                         tweetIds <- Array.append tweetIds [|tweet.id|]
                         tweetsByMention <- tweetsByMention.Add(mention, tweetIds)
                 
-                let pushTweet (tweet: Tweet) =
+                let pushTweet (tweet: Tweet) = // push this live tweet to online users if they have subscribed
                     // Send tweets to online users
                     if subscribersMap.ContainsKey(tweet.by.username) then
                         for subscriber in subscribersMap.Item(tweet.by.username) do
@@ -153,18 +156,18 @@ let main () =
                             // printfn "User %s logged out" user.username
                         | Tweet tweet ->
                             currentTweetCount <- currentTweetCount + 1.0
-                            if tweet.tType = "tweet" then
+                            if tweet.tType = "tweet" then // if new tweet
                                 // printfn "New Tweet [%s] by [%s]" tweet.text tweet.by.username
                                 addTweet tweet
                                 pushTweet tweet
-                            else // retweet
+                            else // if it is a retweet
                                 if tweetsMap.ContainsKey(tweet.reId) then
                                     let retweet = {tweet with text=tweetsMap.Item(tweet.reId).text}
                                     // printfn "ReTweet [%s] by [%s]" tweet.text tweet.by.username
                                     addTweet retweet
                                     pushTweet retweet
                             
-                        | Subscribe subscribe ->
+                        | Subscribe subscribe -> // subscribe to a publisher
                             if not (subscriptionsMap.ContainsKey(subscribe.subscriber)) then    
                                 let mutable subscriptions = if subscriptionsMap.ContainsKey(subscribe.subscriber) then Set.toArray(subscriptionsMap.Item(subscribe.subscriber)) else [||]
                                 subscriptions <- Array.append subscriptions [|subscribe.publisher|]
@@ -176,27 +179,27 @@ let main () =
                                 subscribers <- Array.append subscribers [|subscribe.subscriber|]
                                 subscribersMap <- subscribersMap.Add(subscribe.publisher, subscribers)
                                 // printfn "%s subscribed to %s" subscribe.subscriber subscribe.publisher
-                        | Query query ->
+                        | Query query -> // query
                             printfn "Query from %s" query.by.username
-                            let mutable response: Tweet array = Array.empty
+                            let mutable response: Tweet array = Array.empty // response
                             match query.qType with
-                            | "subscription" ->
+                            | "subscription" -> // my subscriptions
                                 let subscriptions = if subscriptionsMap.ContainsKey(query.by.username) then Set.toArray(subscriptionsMap.Item(query.by.username)) else [||]
                                 for publisher in subscriptions do
                                     let tweetIds = if tweetsByUsername.ContainsKey(publisher) then tweetsByUsername.Item(publisher) else [||]
                                     for tweetId in tweetIds do
                                         response <- Array.append response [|tweetsMap.Item(tweetId)|]
-                            | "hashtag" ->
+                            | "hashtag" -> // tweets of a given hashtag
                                 let tweetIds = if tweetsByHashTag.ContainsKey(query.qName) then tweetsByHashTag.Item(query.qName) else [||]
                                 for tweetId in tweetIds do
                                     response <- Array.append response [|tweetsMap.Item(tweetId)|]
-                            | "mention" ->
+                            | "mention" -> // tweets containing a user's username
                                 let tweetIds = if tweetsByMention.ContainsKey(query.qName) then tweetsByMention.Item(query.qName) else [||]
                                 for tweetId in tweetIds do
                                     response <- Array.append response [|tweetsMap.Item(tweetId)|]
                             | _ -> printfn "Invalid Query Type"
                             sender <! QueryResponse response
-                        | PrintStats ->
+                        | PrintStats -> // Print Stats
                             printfn "----------STATS----------"
                             printfn "Total Tweets: %f" currentTweetCount
                             printfn "Tweets per second: %f" ((currentTweetCount-lastTweetCount)/statsInterval)
@@ -207,11 +210,10 @@ let main () =
                         return! loop()
                     }
                 loop()
+    // print stats every statsInterval seconds
     system.Scheduler.ScheduleTellRepeatedlyCancelable(TimeSpan.Zero, TimeSpan.FromSeconds(statsInterval), server, PrintStats, server) |> ignore
 
-
     async {
-        // let! response = server <? ()
         System.Console.ReadLine() |> ignore
         printfn ""
     } |> Async.RunSynchronously
